9cc481a61d932f02970ed543be89ea87235de139
bmlancien <bryan.lancien@rl-institut.de>
2021-05-19 17:06:02 +0200

    set up empty open_plan repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# WAM-HP
+
+## Dart Sass
+https://sass-lang.com/documentation/cli/dart-sass
+
+### Watch SCSS
+`sass --watch stylesheets/scss:stylesheets/css`
---
+++ js/project.js
@@ -0,0 +1,11 @@
+// Lazy loading images
+
+var lazyLoadInstance = new LazyLoad({
+  // Your custom settings go here
+});
+
+// Animations on scroll
+AOS.init({
+  duration: 1000,
+  disable: "mobile"
+});
