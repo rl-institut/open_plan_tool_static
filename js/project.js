60b51c63f7e62abee34e44f6f16635d550e8302c
bmlancien <bryan.lancien@rl-institut.de>
2021-06-03 10:23:10 +0200

    toggle HIDE/SHOW scenarios

--- js/project.js
+++ js/project.js
@@ -8,3 +8,20 @@ AOS.init({
   duration: 1000,
   disable: "mobile"
 });
+
+////////////////////////////////////////////// My projects page
+const projectWrap = document.querySelector('.projects');
+
+// toggle HIDE/SHOW SCENARIOS text
+projectWrap.addEventListener('click', function() {
+  const collapsedScenariosBoxesExpanded = document.querySelectorAll('.btn.btn--action[aria-expanded="true"] .js-toggle-scenario-name');
+  const collapsedScenariosBoxes = document.querySelectorAll('.btn.btn--action[aria-expanded="false"] .js-toggle-scenario-name');
+
+  collapsedScenariosBoxesExpanded.forEach(function(item) {
+    item.innerHTML = "Hide scenarios";
+  });
+
+  collapsedScenariosBoxes.forEach(function(item) {
+    item.innerHTML = "Show scenarios";
+  });
+})
