3893881b853585aad1e842ee51d09e4aed772831
bmlancien <bryan.lancien@rl-institut.de>
2021-06-15 16:11:02 +0200

    remove lazyload

--- js/project.js
+++ js/project.js
@@ -1,8 +1,3 @@
-////////////////////////////////////////////// Lazy loading images
-var lazyLoadInstance = new LazyLoad({
-  // Your custom settings go here
-});
-
 ////////////////////////////////////////////// Animations on scroll
 AOS.init({
   duration: 1000,
